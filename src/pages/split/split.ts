f2414cab8add2e897a8ae8ff1070a958bd3f8fc7
Sean McGillis <slcidevice@yahoo.com>
2018-03-13 22:11:28 -0600

    changed the name of the denomination property because I no longer use the small blind for it. Also changed the interpolation to reflect denomination is the passed through property.

--- src/pages/split/split.ts
+++ src/pages/split/split.ts
@@ -8,26 +8,23 @@ import { ParentComponent } from '../../parent-classes/parent-component';
 })
 export class SplitPage extends ParentComponent {
   pot: string;
-  smallBlind: string;
+  denomination: string;
   splitFactor: string;
   splitAmount: string;
   extraAmount: string;
   extraChips: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public platform: Platform, public alertCtrl: AlertController) {
-    // If we navigated to this page, we will have an item available as a nav param
     super(platform, alertCtrl);
     this.pot = navParams.get('pot');
-    this.smallBlind = navParams.get('smallBlind');
+    this.denomination = navParams.get('denomination');
     this.splitFactor = navParams.get('splitFactor');
-    // this.isQualifyingLow();
-    console.log(this.smallBlind, this.pot, this.splitFactor);
     this.split();
 
   }
 
     split() {
-    var denomination = Number(this.smallBlind);
+    var denomination = Number(this.denomination);
     var players = Number(this.splitFactor);
     var potTotal = Number(this.pot);
 
@@ -48,22 +45,9 @@ export class SplitPage extends ParentComponent {
         }
     }
 
-  //   while( potTotal >= ( denomination * players ) ){
-  //     for( var i = 1; i <= players; i++ ){
-  //         var playerName = 'player ' + i;
-  //         playerPortions[playerName] += denomination;
-  //         potTotal -= denomination;
-  //     }
-  // }
- 
     this.extraAmount = potTotal.toString();
     this.splitAmount = ( playerPortions[playerName] );
     this.extraChips = (potTotal / denomination).toString();
-    
-    console.log('remaining pot: ' + potTotal );
-    console.log('remaining pot: ' + potTotal / denomination + "  Chips remaining");
-    console.log('each player gets', this.splitAmount); //@DEBUG
-    console.log('this much is left over', this.extraAmount); //@DEBUG
   }
 
   toHome() {
